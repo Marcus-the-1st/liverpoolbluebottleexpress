19255f63d6bd61ea1215f1debd0fb74cff085e33
Marcus-the-1st <mjalbrightson@gmail.com>
2026-09-22 10:46:17 +0200

    Correct NRB category label to Six-Packs of NRB

--- catalog.js
+++ catalog.js
@@ -95,7 +95,7 @@
     const groupsMap = new Map();
     groupsMap.set("six-pack-nrb", {
       id: "six-pack-nrb",
-      name: "Six-Pack of NRB",
+      name: "Six-Packs of NRB",
       emoji: "🍺",
       items: []
     });
